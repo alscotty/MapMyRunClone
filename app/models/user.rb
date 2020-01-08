43fbbdf7c75ed4b7de520f4d5c9fca32208d8247
alscotty <alawrencescott@gmail.com>
2020-01-08 13:37:12 -0800

    mocss

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :username, :password_digest, :session_token, presence: true
   validates :username, uniqueness: true
-  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :password, length: { minimum: 6, message:'must be at least 6 characters' }, allow_nil: true
 
   after_initialize :ensure_session_token
 
