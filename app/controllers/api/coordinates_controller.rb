f53508f5dba543fe987029f6db85d8fa12f00450
alscotty <alawrencescott@gmail.com>
2020-01-10 11:13:03 -0800

    controllers update!

--- app/controllers/api/coordinates_controller.rb
+++ app/controllers/api/coordinates_controller.rb
@@ -1,7 +1,28 @@
 class Api::CoordinatesController < ApplicationController
 
+def new
+end
 
+def show
+    @coordinate=Coordinate.find(params[:id])
+end
 
+def create
+    @coordinate=Coordinate.new(coordinate_params)
+    if @coordinate.save
+        render '/api/coordinate/show'
+    else
+        render json: @coordinate.errors.full_messages, status:422
+    end
+end
+
+def destroy
+end
+
+private
+def coordinate_params
+    params.require(:coordinate).permit(:route_id,:lat,:lng,:ord)
+end
 
 
 end
--- app/controllers/api/routes_controller.rb
+++ app/controllers/api/routes_controller.rb
@@ -6,6 +6,22 @@ end
 def create
     @route=Route.new(route_params)
     if @route.save
+        
+        coordinate_params.each do |idx,coord|
+            coord_obj={
+                lat: coord[:lat],
+                lng: coord[:lng],
+                ord: idx,
+                route_id:@route.id
+            }
+
+            @new_coord=Coordinate.new(coord_obj)
+            if !@new_coord.save
+                render @new_coord.errors.full_messages, status:422
+            end
+        end
+        
+        
         render 'api/routes/show'
     else
         render json: @route.errors.full_messages, status:422
@@ -20,7 +36,6 @@ def index
     @routes=Route.all
 end
 
-#double check later on
 def destroy
     @route=Route.find(params[:id])
     @route.destroy
@@ -32,5 +47,8 @@ def route_params
     params.require(:route).permit(:title, :user_id)
 end
 
+def coordinate_params
+    params.require(:coordinates)
+end
 
 end
